45a4f6ade7c6dcf9596714738ab7fbebbeb104b2
amarjeet <amarjeet@singh.com>
2023-01-07 21:10:58 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// Variable declarations 
+const a = 12; // Once declared it's value can't be changed also it's assignment done at the time of declaration
+let b = 12; // It's value can be chnaged
+var c = 18; // This way also we can declare the variable but the common use coders use is const and let
+
+/*
+console.warn("Its a warning");
+console.error("Its an error");
+window.alert("It's an alert");
+*/
+
+let arr = ["Apple", "Mango", "Grapes"];
+console.log(arr);
+arr.push("Dog");
+console.log(arr);
+
+// In JS array we can add value of different type
+arr.push(100);
+console.log(arr);

0502dd6c2bf34c29a074f26c110047ea0d425689
amarjeet <amarjeet@singh.com>
2023-01-08 14:50:10 +0530

    modified

--- main.js
+++ main.js
@@ -14,6 +14,70 @@ console.log(arr);
 arr.push("Dog");
 console.log(arr);
 
+// We acn access the array by index 
+console.log(arr[1]);
+
 // In JS array we can add value of different type
 arr.push(100);
 console.log(arr);
+
+// Objects 
+const person = {
+    name: "Amarjeet",
+    age: 21,
+    sex: "male",
+    //we can also declare objects inside a object
+    address: {
+        vill: "Jogiara",
+        Dis: "Darbhanga",
+        Hno:  20
+    }
+}
+
+// Accessing the element which is inside the object of a object
+console.log(person.address.vill);
+
+// destructuring in JS | It's a new feature introduced in ES6
+const {name, age} = person;
+console.log(age);
+
+// adding a new thing in existing object
+person.email= "amarjeet@gmail.com"
+console.log(person);
+
+// declaring objects inside array
+const todoarray = [
+    {
+        id: 1,
+        text: "Bath",
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: "Eat",
+        isCompleted: false
+    }
+];
+
+console.log(todoarray);
+
+// access partcular element of an array
+console.log(todoarray[1].text);
+
+// converting object to JSON format
+const todoJSON = JSON.stringify(todoarray);
+console.log(todoJSON);
+
+// For loop in JavaScript
+for(let i=0;i<2;i++)
+console.log(todoarray[i].text);
+
+// While loop
+let j=0;
+while(j<2)
+{
+    console.log(todoarray[j].id);
+    j++;
+}
+
+// map and filter concepts are also there in JS
